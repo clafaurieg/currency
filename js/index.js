3a07dc394ebefbc965186ebc89766e34bfc4c5c7
clafaurieg <70864691+clafaurieg@users.noreply.github.com>
2022-04-09 15:53:15 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,3 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", function (){
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach( n => n.addEventListener("click", function(){
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
-
-const readMoreBtn = document.querySelector('.read-more-btn');
-const text = document.querySelector('.read-more-text');
-
-readMoreBtn.addEventListener("click", function(e){
-    text.classList.toggle('show-more');
-    if (readMoreBtn.innerText === "Read On"){
-        readMoreBtn.innerText = "Read Less";
-    }
- else {
-    readMoreBtn.innerText = "Read More";
-  }
-})
-
-function openDonation(evt, dollarAmount) {
-    var i;
-    donationAmount = document.getElementsByClassName ("donation-amount");
-    for (i = 0; i < donationAmount.length; i++) {
-        donationAmount[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i=0; i<tablinks.length; i++) {
-    }
-    document.getElementById(dollarAmount).style.display = "block";
-}
-
 const currencyEl_one = document.getElementById('currency-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_one = document.getElementById('amount-one');
@@ -49,7 +11,7 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  fetch(`https://v6.exchangerate-api.com/v6/6d7fdfaf4490b6cd52360f33/latest/USD`)
+  fetch(`https://v6.exchangerate-api.com/v6/6d7fdfaf4490b6cd52360f33/latest/${currency_one}`)
     .then((res) => res.json())
     .then((data) => {
       //   console.log(data);
